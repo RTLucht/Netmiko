d9b9f148878b94e6ad5d71970ed7e6df35e16457
Richard T Lucht <78278798+RTLucht@users.noreply.github.com>
2021-01-31 14:46:54 -0600

    Add files via upload
    What can be more fun that figuring out what versions of switch code and authentication mode you are dealing with.  Dealing with multiple agencies that used to do their own thing can be a headache some time.  You may have legacy switches then you may have C3PL, 16.3.8 or 3.5.0.  I get a list of switch IPs to configure and heck with doing them one at a time.  I run that list through this and make 3 categories because variety.

---
+++ SW Version and Auth mode.py
@@ -0,0 +1,60 @@
+import json
+from netmiko import ConnectHandler
+from operator import itemgetter
+from getpass import getpass
+from netmiko.ssh_exception import NetMikoTimeoutException
+from paramiko.ssh_exception import SSHException
+from netmiko.ssh_exception import AuthenticationException
+
+username = input('Enter your username: ')
+password = getpass()
+
+with open('H:\Scripts\TEXTFSM\device_file.txt') as f:
+    devices_list = f.read().splitlines()
+
+device_details = {}
+
+for devices in devices_list:
+    ip_address_of_device = devices
+    SW = {
+        'device_type': 'cisco_ios',
+        'ip': ip_address_of_device, 
+        'username': username,
+        'password': password
+    }
+
+    try:
+        net_connect = ConnectHandler(**SW)
+    except (AuthenticationException):
+        print ('Authentication failure: ' + ip_address_of_device)
+        continue
+    except (NetMikoTimeoutException):
+        print ('Timeout to device: ' + ip_address_of_device)
+        continue
+    except (EOFError):
+        print ('End of file while attempting device ' + ip_address_of_device)
+        continue
+    except (SSHException):
+        print ('SSH Issue. Are you sure SSH is enabled? ' + ip_address_of_device)
+        continue
+    except Exception as unknown_error:
+        print ('Some other error: ' + str(unknown_error))
+        continue
+
+    
+
+    outputs = net_connect.send_command('show version', use_textfsm=True)
+
+    #print(json.dumps(output, indent=2))
+
+    output1 = net_connect.send_command('authentication display config-mode')
+
+    #print(output1)
+
+    for output in outputs:
+            print( f"{output['hostname']} is running {output['version']} and is " + output1)
+            results = ( f"{output['hostname']} is running {output['version']} and is " + output1)
+            file = open('H:\Scripts\TEXTFSM\switches_for_nac.txt','a')
+
+            file.write(results)
+            file.close()
